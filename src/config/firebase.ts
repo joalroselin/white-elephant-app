f4eb30e00a3e6c24b6011b9f1ea16c90093c01c7
Joal Rose Lin <105606368+joalroselin@users.noreply.github.com>
2025-12-10 05:28:36 +0800

    Analytics var fix

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -24,7 +24,7 @@ const firebaseConfig = {
 
 console.log("API Key Check:", firebaseConfig.apiKey);
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 
 const googleProvider = new GoogleAuthProvider();
